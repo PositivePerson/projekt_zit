279eb0b52a980a5179c657ebb0a3d36ad973f02a
Kidarok <karolsocha32@gmail.com>
2023-12-12 22:20:38 +0100

    + login, users

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+# Proste bazy danych użytkowników i administratorów (na potrzeby przykładu)
+users = {
+    'user': {'password': 'userpassword'},
+    'admin': {'password': 'adminpassword'}
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/welcome.html')
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users:
+        if password == users[username]['password']:
+            if username == 'admin':
+                return redirect(url_for('admin_panel'))
+            else:
+                return redirect(url_for('welcome'))
+    
+    return redirect(url_for('index'))  # Przekierowanie z powrotem do strony logowania w przypadku niepowodzenia
+
+@app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+
+@app.route('/admin')
+def admin_panel():
+    return render_template('admin.html')
 
 @app.route('/video.html')
 def video():
